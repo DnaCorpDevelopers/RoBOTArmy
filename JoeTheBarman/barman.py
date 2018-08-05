09ce59372d472e2c62609a8bff592a676c43cc2e
C.B <cbonesana@gmail.com>
2018-08-05 17:10:41 +0200

    Added more drinks to Joe!

--- JoeTheBarman/barman.py
+++ JoeTheBarman/barman.py
@@ -7,12 +7,13 @@ from BasicBot.basic import BasicBot, log
 from JoeTheBarman.languages.it.conversations import *
 from JoeTheBarman.languages.it.drinks import initialDrinkList
 
-envConfig = 'JoeTheBarman/resources/environment.config'
+ENV_CONFIG = 'JoeTheBarman/resources/environment.config'
+IMAGES = 'JoeTheBarman/images/'
 
 
 class Barman(BasicBot):
     def __init__(self, client: Client):
-        super().__init__(client, envConfig)
+        super().__init__(client, ENV_CONFIG)
 
         self.user = client.user
         self.drinks = initialDrinkList
@@ -60,7 +61,7 @@ class Barman(BasicBot):
         log.debug('tokens: ' + str(tokens))
 
         # search for any kind of drink
-        foundDrinks = [d for d in self.drinks if d['key'] in clean]
+        foundDrinks = [d for d in self.drinks if d['key'] in tokens]
         mention = message.author.mention
         title = random.choice(titles)
 
@@ -68,28 +69,28 @@ class Barman(BasicBot):
             log.info('send generic order request')
             return random.choice(orders).format(message.author.mention), None
 
-        if 'grazie' in tokens:
+        if len([a for a in thankAnswer if a in tokens]) > 0:
             # get a random thank string
             log.info('send thanks')
             return random.choice(thanks).format(title), None
 
-        if 'drinks' in tokens:
+        if len([d for d in drinkList if d in tokens]) > 0:
             # return the list of drinks
             log.info('send drink list')
-            return 'Abbiamo ' + ', '.join([d['name'] for d in self.drinks]), None
+            return random.choice(drinkListIncipit) + '\n - '.join([d['name'] for d in self.drinks]), None
 
         if len(foundDrinks) < 1:
             # if we haven't found a valid drink
-            log.info('send not found')
+            log.info('send not found ' + str(tokens))
             return random.choice(notFound), None
 
         # we have found a drink!
         foundDrink = foundDrinks[0]
 
-        if 'tutti' in tokens or message.mention_everyone:
+        if everybodyRequest in tokens or message.mention_everyone:
             # if it is for @everyone or contains a special token
             log.info('send offer to everyone')
-            return '@everyone il prossimo giro di {2} lo offre {1} {0}!'.format(
+            return random.choice(sendToEveryone).format(
                 mention, title, foundDrink['name']
             ), foundDrink['img']
 
@@ -107,7 +108,9 @@ class Barman(BasicBot):
         conclusion = random.choice(conclusions)
 
         log.info('sending drink')
-        return intro + ' ' + drink + ' ' + conclusion, foundDrink['img']
+        log.info('drink: ' + drink)
+        log.info('to: ' + mention)
+        return intro + ' ' + drink + ' ' + conclusion, IMAGES + foundDrink['img']
 
     async def login(self):
         await super().login()
--- JoeTheBarman/languages/it/conversations.py
+++ JoeTheBarman/languages/it/conversations.py
@@ -1,9 +1,44 @@
 names = ['joe', 'barista', 'barman', 'ehi', 'yo', 'hey', 'hei', 'jo']
 
-titles = ['', ' Messere', ' Sir', ' Comandante']
+titles = ['', ' Messere', ' Sir', ' Comandante', 'Mozzo']
 
 greetings = [
-    'Signori e signore, il bar è ora aperto!'
+    'Signore e signori, il bar è ora aperto!',
+    'Gentili clienti, è arrivata nuova merce!',
+    'Gentaglia! È qui la festa!'
+]
+
+thankAnswer = [
+    'grazie',
+    'gentile',
+    'mancia',
+    'resto'
+]
+
+drinkList = [
+    'drinks',
+    'servi'
+    'menu',
+    'carta'
+]
+
+drinkListIncipit = [
+    'Abbiamo: ',
+    'A lei: ',
+    'Ecco: ',
+    'Solo il meglio per voi:'
+]
+
+everybodyRequest = [
+    'tutti',
+    'ciurma'
+]
+
+sendToEveryone = [
+    '@everyone il prossimo giro di {2} lo offre {1} {0}!',
+    'Sentito ciurma? {1} {0} offre a @everyone il prossimo giro di {2}!',
+    'Informiamo la gentile clientela che {1} {0} offre a @everyone {2}!',
+    '@everyone , il prossimo TS di {2} è offerto da {1} {0}!'
 ]
 
 orders = [
@@ -15,14 +50,21 @@ orders = [
     'Comandante {}, sono a sua disposizione.',
     'Ah, {},... mi dica, ma in fretta.',
     'Come posso essere utile?',
-    'Di nuovo?'
+    'Di nuovo?',
+    'Sono a sua disposizione.',
+    'Eccomi!',
+    'Il solito?',
+    'Bocca asciutta?'
 ]
 
 thanks = [
     'Prego!',
     'Di nulla!',
     'Alla prossima!',
-    'Grazie {}, ma sono solo un umile barman ;)'
+    'Grazie {}, ma sono solo un umile barman ;)',
+    'Troppo gentile!',
+    'Infinite grazie {}',
+    'Troppo buono :D '
 ]
 
 concorde = [
@@ -39,14 +81,24 @@ incipits = [
     'In arrivo all\'hangar 42',
     'Specialità della casa:',
     'Dalla riserva speciale',
-    'Solo per lei Comandante {}'
+    'Solo per lei Comandante {}',
+    'Ecco a lei,',
+    'Tutto suo,',
+    'Appena scongelato, ',
+    'Appena stappato, ',
+    'È lì da un po\'... credo, ',
+    'Non me lo chiede mai nessuno, ',
+    'Direttamente dal nostro serbatoio, ',
+    '(Sputa nel bicchiere) ecco, '
 ]
 
 drinkings = [
     "un buon bicchiere di {}",
     "una bottiglia di {} dell'annata buona",
     "un fusto di {}",
-    "ho appena aperto una bottiglia di {}"
+    "ho appena aperto una bottiglia di {}",
+    "un bicchiere di {} con l'ombrellino",
+    "un bicchiere di {} senza l'ombrellino (li ho finiti!)"
 ]
 
 conclusions = [
@@ -54,7 +106,11 @@ conclusions = [
     ", ci vuole anche del ghiaccio?",
     ", altro?",
     ", la metto sul conto del boss?",
-    ", offre la casa!"
+    ", offre la casa!",
+    ", offre la ditta!",
+    ", fresco fresco (eheh).",
+    ", salute!",
+    ", cin-cin!"
 ]
 
 notFound = [
@@ -66,5 +122,12 @@ notFound = [
     'Arriva martedì',
     'Purtroppo qui non serviamo quella robaccia...',
     'Questo è un bar di un certo livello!',
-    'Mi ritengo offeso da questa richiesta!'
+    'Mi ritengo offeso da questa richiesta!',
+    'Il prossimo?',
+    'Avanti un altro!',
+    'Mi dispiace, è stato riservato per un\'occasione speciale.',
+    'Non mi sembra in grado di reggersi in piedi...',
+    'Fi-ni-to-!',
+    'Esaurito...',
+    'Lo devo ordinare'
 ]
